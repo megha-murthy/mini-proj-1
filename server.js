5b9193027d974a768bc1beaf1d290ab509f644f0
chrismw <chrismw7579@gmail.com>
2020-02-22 12:27:39 -0800

    added server.js

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const path = require('path');
+const Reservation = require('reservation');
+const Restaurant = require('restaurant');
+const app = express();
+const PORT = 3000;
+
+
+const restaurant = new Restaurant();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.post('/make', (req, res) => {
+    const newReservation = new Reservation(req.body);
+    restaurant.addReservation(newReservation);
+});
+
+app.get('/api/table', (req, res) => {
+    res.json(tables);
+});
+
+app.get('/api/reservations', (req, res) => {
+    res.json(reservations);
+});
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, 'home.html'));
+});
+
+app.get('/tables', (req, res) => {
+    res.sendFile(path.join(__dirname, 'view.html'));
+});
+
+app.get('/make', (req, res) => {
+    res.sendFile(path.join(__dirname, 'make.html'));
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
